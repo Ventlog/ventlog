4972987cbd19376ddb9f488588c24eabe607cfa3
Konflict <Konflict@Konflicts-MacBook-Pro.local>
2016-03-03 22:25:09 -0800

    added search bar top left as well as some javascript for post a status

---
+++ post_a_status.js
@@ -0,0 +1,8 @@
+//document.addEventListener('DOMContentLoaded', function() {
+
+function togglediv(id) {
+    var div = document.getElementById('test');
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+//}, false);
