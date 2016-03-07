5bab33b9b403f483a95e97be5e6c1f0775e9e22d
butcallmejo <13@holbertonschool.com>
2016-03-06 22:56:09 -0800

    added the toggle js script and also the reply js

--- post_a_status.js
+++ post_a_status.js
@@ -1,8 +1,5 @@
-//document.addEventListener('DOMContentLoaded', function() {
 
 function togglediv(id) {
-    var div = document.getElementById('test');
-    div.style.display = div.style.display == "none" ? "block" : "none";
+  var div = document.getElementById('test');
+  div.style.display = div.style.display == "none" ? "block" : "none";
 }
-
-//}, false);
