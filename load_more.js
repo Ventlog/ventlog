a4e2a888f51c5296f23e008d4bd6270f08f59df5
butcallmejo <13@holbertonschool.com>
2016-03-18 16:58:15 -0700

    need to test load_more.js

--- load_more.js
+++ load_more.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  function addEvent(el, eventType, handler) {
-    if (el.addEventListener) { // DOM Level 2 browsers
-      el.addEventListener(eventType, handler, false);
-    } else if (el.attachEvent) { // IE <= 8
-      el.attachEvent('on' + eventType, handler);
-    } else { // ancient browsers
-      el['on' + eventType] = handler;
-    }
-  }
-
   var button = document.getElementById("loadMoreBttn");
-  addEvent(button, 'click', function() {
-    alert("Hello");
-  });
 
-  addEvent(button, 'click', function() {
-    alert("world!");
-  });
-
-
-  function loadMoreStatuses() {
+  button.onclick = function loadMoreStatuses() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-        document.getElementById("extra_statuses").innerHTML = xhttp.responseText;
+        document.getElementById("extra_statuses").innerHTML = xhttp.response;
       }
     };
-    xhttp.open("GET", "statuses-1.html", true);
+    xhttp.open("GET", "/statuses-1.html", true);
     xhttp.send();
   }
 }
+
+// function addEvent(el, eventType, handler) {
+//   if (el.addEventListener) { // DOM Level 2 browsers
+//     el.addEventListener(eventType, handler, false);
+//   } else if (el.attachEvent) { // IE <= 8
+//     el.attachEvent('on' + eventType, handler);
+//   } else { // ancient browsers
+//     el['on' + eventType] = handler;
+//   }
+// }
+
+// addEvent(button, 'click', function() {
+//   alert("Hello");
+// });
+//
+// addEvent(button, 'click', function() {
+//   alert("world!");
+// });
