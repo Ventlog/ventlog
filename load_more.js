5d8a910537eac9aea009a96ecbce37c71b44cf70
butcallmejo <13@holbertonschool.com>
2016-03-18 17:21:02 -0700

    load_more worksgit add .git add .!

--- load_more.js
+++ load_more.js
@@ -1,18 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
 
   var button = document.getElementById("loadMoreBttn");
 
-  button.onclick = function loadMoreStatuses() {
+  button.onclick = function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
+        //alert("test is this working?")
         document.getElementById("extra_statuses").innerHTML = xhttp.response;
       }
     };
-    xhttp.open("GET", "/statuses-1.html", true);
+    xhttp.open("GET", "statuses-1.html", true);
     xhttp.send();
   }
-}
+});
 
 // function addEvent(el, eventType, handler) {
 //   if (el.addEventListener) { // DOM Level 2 browsers
