5ea26868976360fd6eada54e61913b5babfe2a02
butcallmejo <13@holbertonschool.com>
2016-03-22 16:20:15 -0700

    finished mandatory tasks

--- ajax.js
+++ ajax.js
@@ -1,10 +1,13 @@
 function ajaxGet(url, onSuccess) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      onSuccess(xhttp.responseText)
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
+  document.getElementById('loadMoreBttn').disabled = true;
+  setTimeout(function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        onSuccess(xhttp.response)
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }, 2000);
 }
--- load_more.js
+++ load_more.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
   var button = document.getElementById("loadMoreBttn");
 
   button.onclick = function () {
-    ajaxGet(statuses-1.html, onSuccess(resp))
-    document.getElementById("extra_statuses").innerHTML = resp;
+    ajaxGet("statuses-1.html", function(str) {
+      document.getElementById("extra_statuses").innerHTML = str;
+    });
+
     // var xhttp = new XMLHttpRequest();
     // xhttp.onreadystatechange = function() {
     //   if (xhttp.readyState == 4 && xhttp.status == 200) {
